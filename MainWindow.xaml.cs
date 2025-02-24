a164e232fbe9bbcf8d198178a609e19dd455a042
prionz1 <112930799+prionz1@users.noreply.github.com>
2025-02-24 10:38:08 +0100

    Commit #2: Jelentes osztály és adatbeolvasás

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,12 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 
 namespace Autok
 {
     public partial class MainWindow : Window
     {
+        private List<Jelentes> jelentesek = new List<Jelentes>();
+
         public MainWindow()
         {
             InitializeComponent();
+            BeolvasAdatok();
+        }
+
+        private void BeolvasAdatok()
+        {
+            string filePath = "jeladas.txt"; 
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("A jeladas.txt nem található!");
+                return;
+            }
+
+            var sorok = File.ReadAllLines(filePath);
+            foreach (var sor in sorok)
+            {
+                var darabok = sor.Split('\t'); // tabulátoros feldolgozás
+                if (darabok.Length >= 4)
+                {
+                    string rendszam = darabok[0];
+                    int ora = int.Parse(darabok[1]);
+                    int perc = int.Parse(darabok[2]);
+                    int sebesseg = int.Parse(darabok[3]);
+
+                    var jel = new Jelentes(rendszam, ora, perc, sebesseg);
+                    jelentesek.Add(jel);
+                }
+            }
+
+            MessageBox.Show($"Sikeres beolvasás, {jelentesek.Count} sor érkezett.");
         }
     }
 }
